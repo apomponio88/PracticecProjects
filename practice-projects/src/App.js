4277921bacaa8e8e479a2bde47aea62fa2fbcab0
apomponio <apomponio@gmail.com>
2023-01-08 18:44:37 -0500

    please dont break

---
+++ practice-projects/src/App.js
@@ -0,0 +1,110 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css'
+import { useReducer } from "react"
+import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
+
+export const ACTIONS= {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUaTE: 'evaluate'
+}
+
+function reducer(state, {type, payload}){
+  // eslint-disable-next-line default-case
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      if(payload.digit === "0" && state.currentOperand === "0") return state;
+      if(payload.digit === "." && state.currentOperand.includes(".")) 
+      return state;
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand} || "" ${payload.digit}`,
+      }
+      case ACTIONS.CHOOSE_OPERATION:
+        if(state.currentOperand == null && state.previousOperand == null) {
+          return state
+        }
+        if(state.previousOperand == null) {
+          return {
+            ...state,
+            operation: payload.operation,
+            previousOperand: state.currentOperand,
+            currentOperand:null
+          }
+        }
+        return {
+          ...state,
+          previousOperand: evaluate(state),
+          currentOperand: null,
+          operation: payload.operation
+        }
+      case ACTIONS.CLEAR:
+        return {}
+  }
+}
+
+function evaluate({ currentOperand, previousOperand, operation}) {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if(isNaN(prev) || isNaN(current)) return ""
+  let computation = ""
+  // eslint-disable-next-line default-case
+  switch (operation) {
+    case "+":
+    computation = prev + current
+       break
+    case "-":
+      computation = prev - current
+      break
+      case "/":
+    computation = prev / current
+      break
+    case "*":
+    computation = prev * current
+      break
+  }
+  return computation.toString;
+}
+
+
+function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(
+    reducer,
+    {}
+    )
+  
+  
+  
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <button className="span-two" onClick={() => dispatch ({type: ACTIONS.CLEAR})}>AC</button>
+      <button>DEL</button>
+      <OperationButton operation="/" dispatch={dispatch}/>
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
+      <OperationButton operation="*" dispatch={dispatch}/>
+      <DigitButton digit="4" dispatch={dispatch}/>
+      <DigitButton digit="5" dispatch={dispatch}/>
+      <DigitButton digit="6" dispatch={dispatch}/>
+      <OperationButton operation="+" dispatch={dispatch}/>
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
+      <OperationButton operation="-" dispatch={dispatch}/>
+      <DigitButton digit="." dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch}/>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ practice-projects/src/OperationButton.js
@@ -0,0 +1,10 @@
+import { ACTIONS } from "./App"
+
+export default 
+function OperationButton({ dispatch, digit}) {
+    return (<button 
+    onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: {digit}})} >
+        {digit}
+        </button>
+    )
+}
